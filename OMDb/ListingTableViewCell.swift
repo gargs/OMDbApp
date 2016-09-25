ae324e53befc1ff05790344e4e1d88c6349b67d9
Saurabh Garg <saurabh@weekcal.com>
2016-09-25 21:41:34 +0200

    - adds a prototype cell for search results

---
+++ OMDb/ListingTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ListingTableViewCell.swift
+//  OMDb
+//
+//  Created by Saurabh Garg on 25/09/2016.
+//  Copyright © 2016 Gargs. All rights reserved.
+//
+
+import UIKit
+
+class ListingTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
--- OMDb/SearchResultsTableViewController.swift
+++ OMDb/SearchResultsTableViewController.swift
@@ -8,9 +8,26 @@
 
 import UIKit
 
-class SearchResultsTableViewController: UITableViewController {
+class SearchResultsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 80
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListingCell", for: indexPath)
+        return cell
     }
 }
