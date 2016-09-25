dc58fe9f4890c9b3873de91e7176428f73cbf251
Saurabh Garg <saurabh@weekcal.com>
2016-09-25 20:09:23 +0200

    - adds a search results table view controller

---
+++ OMDb/SearchResultsTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchResultsTableViewController.swift
+//  OMDb
+//
+//  Created by Saurabh Garg on 25/09/2016.
+//  Copyright © 2016 Gargs. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
