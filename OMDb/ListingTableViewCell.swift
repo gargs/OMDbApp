ba8ccd399b5ec7882af248d74d8c6af51008b28f
Saurabh Garg <saurabh@weekcal.com>
2016-09-27 00:33:09 +0200

    - adds working search and thumbnails

--- OMDb/ListingTableViewCell.swift
+++ OMDb/ListingTableViewCell.swift
@@ -13,4 +13,18 @@ class ListingTableViewCell: UITableViewCell {
     @IBOutlet weak var thumbnailImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     
+    var thumbnailURL: URL? {
+        willSet(newValue) {
+            if let url = newValue {
+                if let data = try? Data(contentsOf: url) {
+                    let image = UIImage(data: data)
+                    thumbnailImageView.image = image
+                } else {
+                    thumbnailImageView.image = UIImage(named: "thumbnail.png")
+                }
+            } else {
+                thumbnailImageView.image = UIImage(named: "thumbnail.png")
+            }
+        }
+    }
 }
--- OMDb/SearchResultsTableViewController.swift
+++ OMDb/SearchResultsTableViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class SearchResultsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var searchTerm: String!
+    var searchResult: SearchResult?
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -21,18 +22,21 @@ class SearchResultsViewController: UIViewController, UITableViewDelegate, UITabl
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 80
         
+        navigationItem.title = searchTerm
+        
         debugPrint("Search Term: " + searchTerm)
         
-        
-        
-        
         let session = URLSession.shared
-        let request = URLRequest(url: URL(string: "http://omdbapi.com/?s=terminator")!)
+        let request = URLRequest(url: searchURL(searchTerm: searchTerm))
         let downloadTask = session.dataTask(with: request) { (data, response, error) in
             if let data = data {
                 if let results = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions(rawValue: 0)) {
                     let searchResults = parse(results)
                     debugPrint(searchResults)
+                    DispatchQueue.main.async {
+                        self.searchResult = searchResults
+                        self.tableView.reloadData()
+                    }
                 }
             }
         }
@@ -44,11 +48,14 @@ class SearchResultsViewController: UIViewController, UITableViewDelegate, UITabl
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 6
+        return searchResult?.movies?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ListingCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListingCell", for: indexPath) as! ListingTableViewCell
+        cell.thumbnailURL = searchResult?.movies?[indexPath.row].posterURL
+        cell.titleLabel.text = searchResult?.movies?[indexPath.row].title
+        
         return cell
     }
 }
